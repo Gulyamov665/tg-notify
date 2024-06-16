5f36ccbd282d89005d0f94424368bee68b6cd0bf
Gulyamov665 <gulyamov640@gmail.com>
2024-06-16 18:01:10 +0500

    add chats2

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ router = APIRouter()
 origins = ["*"]
 
 WEBHOOK_PATH = f"/bot/{TELEGRAM_BOT_TOKEN}"
-# WEBHOOK_URL = "https://tg-notify-devteam-a9d6d4f8.koyeb.app" + WEBHOOK_PATH
-WEBHOOK_URL = "https://4ed5-84-54-82-236.ngrok-free.app" + WEBHOOK_PATH
+WEBHOOK_URL = "https://tg-notify-devteam-a9d6d4f8.koyeb.app" + WEBHOOK_PATH
+# WEBHOOK_URL = "https://4ed5-84-54-82-236.ngrok-free.app" + WEBHOOK_PATH
 
 app.add_middleware(
     CORSMiddleware,
