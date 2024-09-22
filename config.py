76bddfc9e53ac7a24d499a233ee170bbe81f81f6
Gulyamov665 <gulyamov640@gmail.com>
2024-09-22 17:51:45 +0500

    add monday promo2

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TELEGRAM_BOT_TOKEN = "7072322760:AAGrlOjG50GFOZ4cq37NOeI5gRByyuAIUok"
 
 WEBHOOK_PATH = f"/bot/{TELEGRAM_BOT_TOKEN}"
-# WEBHOOK_URL = "https://tg-notify-devteam-a9d6d4f8.koyeb.app" + WEBHOOK_PATH
-WEBHOOK_URL = "https://b7a8-84-54-70-123.ngrok-free.app" + WEBHOOK_PATH
+WEBHOOK_URL = "https://tg-notify-devteam-a9d6d4f8.koyeb.app" + WEBHOOK_PATH
+# WEBHOOK_URL = "https://b7a8-84-54-70-123.ngrok-free.app" + WEBHOOK_PATH
